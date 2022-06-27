f1aa96fb344418287b4aba0d2e9dca0818de5a30
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-06-27 17:44:34 +0200

    Major update
    1) Pulizia codice sui controller
    2) Completato l'elenco degli acquisti effettuati per ogni utente
    3) Bugfix dell'acquisto di un bene non disponibile
    4)Aggiornamenti al file di seeding del db
    5) Gestito l'acquisto multiplo

--- controllers/controller_acquisto.js
+++ controllers/controller_acquisto.js
@@ -13,13 +13,15 @@ class Controller_acquisti {
 
         if(utente.credito<1) return [401, 'ERRORE: credito residuo insufficiente'];
         
-        const prodotto = await Database.prodotto.findOne({where: { id_prodotto: datiProdotto.id_prodotto}});
-        if( ! prodotto) return [404, 'ERRORE: prdotto [' + datiProdotto.id_prodotto + '] non trovato'];       
+        const prodotto = await Database.prodotto.findOne({where: { id_prodotto: datiProdotto.id_prodotto, disponibile: true}});
+        if( ! prodotto) return [404, 'ERRORE: prodotto [' + datiProdotto.id_prodotto + '] non trovato o momentaneamente on disponibile!'];       
         
         const dataAcquisto = getDataCorrente();
-
-        const acquistoSalvato = await Database.acquisto.create({ utente: utente.id_utente, prodotto: datiProdotto.id_prodotto, data_acquisto: dataAcquisto});
-        if( ! acquistoSalvato) return [500, 'ERRORE SERVER: impossibile salvare l\'acquisto'];
+        const acquistoPresente = await Database.acquisto.findOne({where: {utente: utente.id_utente, prodotto: prodotto.id_prodotto}});
+        if( ! acquistoPresente)  {
+          const acquistoSalvato = await Database.acquisto.create({ utente: utente.id_utente, prodotto: datiProdotto.id_prodotto, data_acquisto: dataAcquisto, originale: true});
+          if( ! acquistoSalvato) return [500, 'ERRORE SERVER: impossibile salvare l\'acquisto'];
+        } else return [401, 'ERRORE: impossibile acquistare nuovamente!'];
     
         // aggiorna credito residuo dell'utente
         const creditoResiduo = utente.credito-1;
@@ -34,6 +36,35 @@ class Controller_acquisti {
                        '] ha acquistato con successo [' + prodotto.nome_prodotto + '], ' +
                             'il link è: ' + prodotto.link];
         
+    }
+
+    async acquistoAggiuntivo(decoded, datiProdotto){
+       const utente = await Database.utente.findOne({where: { id_utente: decoded.id_utente }});
+       if( ! utente) return [404, 'ERRORE: utente [' + decoded.id_utente + '] non trovato'];
+
+       if(utente.credito<1) return [401, 'ERRORE: credito residuo insufficiente'];
+       
+       const prodotto = await Database.prodotto.findOne({where: { id_prodotto: datiProdotto.id_prodotto, disponibile: true}});
+       if( ! prodotto) return [404, 'ERRORE: prodotto [' + datiProdotto.id_prodotto + '] non trovato o momentaneamente non disponibile!'];
+       
+       const dataAcquisto = getDataCorrente();
+
+       const acquistoOriginale = await Database.acquisto.findOne({where: {utente: utente.id_utente, prodotto: prodotto.id_prodotto, originale: true}});
+       if( ! acquistoOriginale)  return [401, 'ERRORE: impossibile effettuare un acquisto aggiuntivo di un prodotto non acquistato precedentemente!'];
+      
+       const acquistoSalvato = await Database.acquisto.create({ utente: utente.id_utente, prodotto: datiProdotto.id_prodotto, data_acquisto: dataAcquisto, originale: false});
+       if( ! acquistoSalvato) return [500, 'ERRORE SERVER: impossibile salvare l\'acquisto'];
+      
+       // aggiorna credito residuo dell'utente
+       const creditoResiduo = utente.credito-1;
+       const creditoAggiornato = await Database.utente.update({ credito: creditoResiduo }, {
+           where: {
+             id_utente: utente.id_utente
+           }
+         });
+       if( ! creditoAggiornato) return [500, 'ERRORE SERVER: impossibile aggiornare il credito residuo'];
+   
+       return [200, 'SUCCESSO: l\'utente [' + utente.nome + ' '+ utente.cognome + '] ha diritto ad un download aggiuntivo [' + prodotto.nome_prodotto + '], ' + 'il link è: ' + prodotto.link];
     }
 
 }
--- routes/utenti.js
+++ routes/utenti.js
@@ -10,39 +10,9 @@ const controllerAccesso = new ControllerAccesso();
 const ControllerUtente = require('../controllers/controller_utente');
 const controllerUtente = new ControllerUtente();
 
-/*
+
 // Definizione delle varie rotte per gli utenti
 
-router.get('/', verifyToken, async (req, res) => {
-
-    var result = await userController.getAllUsers();
-    res.status(result[0]).json(result[1]);
-    
-});
-
-
-// ottieni profilo utente corrente
-router.get('/:userID', verifyToken, async (req, res) => {
-
-    var result = await userController.getUser(req.params.userID);
-    res.status(result[0]).json(result[1]);
-
-});
-
-
-// registrazione
-router.post('/register', async (req, res) => {
-
-    // REGISTER VALIDATION: controlla se vengono rispettati i criteri definiti con joi in validation.js
-    const { error } = registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    var result = await authController.register(req.body);
-    res.status(result[0]).send(result[1]);
-
-});
-
-*/
 router.post('/credito', verificaToken, async (req, res) => {
     
     let token = req.header('Authorization');
@@ -60,7 +30,7 @@ router.post('/acquistiUtente', verificaToken, async (req, res) => {
     token = token.split(" ");
     const decoded = jwt.decode(token[1], process.env.TOKEN_SECRET);
     
-    const result = await controllerUtente.getAcquistiUtente(decoded);
+    const result = await controllerUtente.getAcquistiUtente(decoded, req.body);
     res.status(result[0]).json(result[1]);
 
 });
@@ -89,15 +59,4 @@ router.post('/ricaricaUtente', verificaToken, isAdmin, async (req, res) => {
 
 });
 
-/*
-
-// elimina collezione utenti
-router.delete('/', verifyToken, isAdmin, async (req, res) => {
-
-    var result = await userController.deleteAllUsers();
-    res.status(result[0]).send(result[1]);
-
-});
-
-*/
 module.exports = router;
